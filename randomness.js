7af94d390d87be764b32652975fe337e7ebcebf7
giovanniASC6 <giovanni.owens@gmail.com>
2019-07-11 14:21:31 -0400

    randomness

---
+++ randomness.js
@@ -0,0 +1,36 @@
+//////////////////////
+// GLOBAL VARIABLES //
+//////////////////////
+const alphabet = "abcdefghijklmnopqrstuvwxyz";
+const foods = [
+    "pizza",
+    "burgers",
+    "rice and beans",
+    "lasagna",
+    "macaroni and cheese",
+    "baked potatoes"
+];
+const students = [
+    "Bob",
+    "Martin",
+    "Danny",
+    "Mahdi",
+    "Harry",
+    "Sean",
+    "Robert"
+];
+/////////////////////////////
+// END OF GLOBAL VARIABLES //
+/////////////////////////////
+
+function rand() { 
+   const min = 0
+   const max = 21
+    const decimal = Math.random()* (max - min) +min; 
+    const times21 = decimal *multiplier;
+    const final = Math.floor(times21);
+
+    return final
+}
+
+console.log(final + random);
